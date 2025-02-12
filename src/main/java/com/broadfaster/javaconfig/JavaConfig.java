ab25f853f842b2cefb29b5124fc1d1887285279a
Akshat Kumar <akshat20kumar@gmail.com>
2025-02-12 22:02:26 +0530

    bean scope

--- src/main/java/com/broadfaster/javaconfig/JavaConfig.java
+++ src/main/java/com/broadfaster/javaconfig/JavaConfig.java
@@ -3,12 +3,21 @@ package com.broadfaster.javaconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class JavaConfig {
 
-    @Bean(name = {"akshat", "mybean"})
+//    by default the bean name will be same as method name in IOC container but we can specify explicitly
+//    by default bean scope is singleton
+    @Bean(name = {"akshat", "student"})
+    @Scope("prototype")
     public Student getStudent() {
+        return new Student();
+    }
+
+    @Bean(name = "student3")
+    public Student getAnotherStudent() {
         return new Student();
     }
 
--- src/main/java/com/broadfaster/javaconfig/Test.java
+++ src/main/java/com/broadfaster/javaconfig/Test.java
@@ -9,9 +9,16 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
 
-        Student st1 = context.getBean("mybean", Student.class);
+//        we are requesting spring ioc to give us student object as by default it has singleton scope one object will be returned each time we request.
+        Student st1 = context.getBean("student", Student.class);
+        Student st2 = context.getBean("student", Student.class);
 
-        System.out.println(st1);
+        System.out.println("student1 = " + st1 + " HASHCODE : " + st1.hashCode());
+        System.out.println("student2 = " + st2 + " HASHCODE : " + st2.hashCode());
+
+        Student st3 = context.getBean("student3", Student.class);
+
+        System.out.println("student3 = " + st3 + " HASHCODE : " + st3.hashCode());
     }
 
 }
